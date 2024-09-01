f8a2bcb8e8bd2f3ad9e8437dfc42573c471fdaba
Mustafa Akif Motor <makifmotor@hotmail.com>
2024-09-01 21:05:47 +0300

    .

---
+++ DataAccessLayer/Concrete/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly YalcoContext _context;
+        public GenericRepository(YalcoContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
